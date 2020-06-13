d5acccf74eede0f781a6100aa2595b3c0c67439e
gerardodelvalle <gerardo.delvalle@journalism.cuny.edu>
2020-06-12 20:41:32 -0400

    Upload new experiment

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("Paises").ready(function() => {
+  country.addEventListener("mousedown", event => {
+    document.querySelectorAll(".countries").fadeIn(2000);
+    });
+  });
+});
