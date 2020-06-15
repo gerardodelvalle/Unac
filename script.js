f6ba34a14eef11522f916363899206312ccba812
gerardodelvalle <gerardo.delvalle@journalism.cuny.edu>
2020-06-15 18:57:48 -0400

    A LOT of changes
    These are a lot of changes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    document.getElementById("header").style.fontSize = "30px";
-  } else {
-    document.getElementById("header").style.fontSize = "90px";
-  }
+var x = 0;
+function myFunction() {
+  document.getElementById("#opening").innerHTML = x += 1;
 }
 
-document.querySelector("#Guatemala").addEventListener("mouseenter", e => {
-  e.target.style.transform = "scale(1.1)";
-});
-document.querySelector("#Guatemala").addEventListener("mouseleave", e => {
-  e.target.style.transform = "scale(1)";
-});
+//
+// function scrollFunction() {
+//   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+//     document.getElementById("header").style.fontSize = "30px";
+//   } else {
+//     document.getElementById("header").style.fontSize = "90px";
+//   }
+// }
+//
+// document.querySelector("#Guatemala").addEventListener("mouseenter", e => {
+//   e.target.style.transform = "scale(1.1)";
+// });
+// document.querySelector("#Guatemala").addEventListener("mouseleave", e => {
+//   e.target.style.transform = "scale(1)";
+// });
