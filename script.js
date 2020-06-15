9f3f1d4914f6929c50149d512201309e956a08d0
gerardodelvalle <gerardo.delvalle@journalism.cuny.edu>
2020-06-15 15:43:06 -0400

    Stylisitic choices and starting to use javascript
    Some errors, some successes. Everything else

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-document.querySelectorAll("Paises").ready(function() => {
-  country.addEventListener("mousedown", event => {
-    document.querySelectorAll(".countries").fadeIn(2000);
-    });
-  });
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
+
+document.querySelector("#Guatemala").addEventListener("mouseenter", e => {
+  e.target.style.transform = "scale(1.1)";
+});
+document.querySelector("#Guatemala").addEventListener("mouseleave", e => {
+  e.target.style.transform = "scale(1)";
 });
