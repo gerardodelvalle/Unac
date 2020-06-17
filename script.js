02e51513bdbcedc5e7a859885695df6539af4362
gerardodelvalle <gerardo.delvalle@journalism.cuny.edu>
2020-06-17 11:17:35 -0400

    A lot of changes
    Help, I really don't understand Javascript

--- script.js
+++ script.js
@@ -1,53 +1,20 @@
-// var y = 0;
+console.log("Hello world!")
+console.log("scroll")
 
-function myFunction() {
-  document.getElementById("header").innerHTML = x += 1;
+const updateParallax = () => {
+  const scrollPosition = window.scrollY
+  const documentHeight = document.documentElement.scrollHeight - window.innerHeight
+  const pctScroll = scrollPosition / documentHeight
 }
 
-// const updateParallax = () => {
-//   const scrollPosition = window.scrollX
-//   const documentHeight = document.documentElement.scrollHeight - window.innerHeight
-//   const pctScroll = scrollPosition
-//
-// const bg = document.querySelector(".opening")
-//   const newLeft = -1 * pctScroll * (document.documentElement.scrollWidth)
-//   bg.style.left = newLeft + "px"
-//
-//   const fg = document.querySelector("h1")
-//   //const newOpacity = 1 - ((pctScroll - 0.5) / 0.5)
-//   if (pctScroll < 0.5){
-//     newOpacity = 0
-//   } else {
-//     newOpacity = (pctScroll - 0.5) / 0.5
-// }
-// }
-// function scrollFunction() {
-//   console.log("scrolling")
-//   if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50)
-//   {
-//     document.getElementById("header").style.fontSize = "60pt";
-//   } else {
-//     document.getElementById("header").style.fontSize = "40pt";
-//     document.getElementById("header").style.transition = "2s";
-//   }
-// }
-// window.addEventListener("scrolling", scrollFunction)
-//
+window.onscroll = function() {scrollFunction()};
+
 function scrollFunction() {
-  console.log("scroll")
-  if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50)
-  {
-    document.getElementById("subtitle").style.fontSize = "60pt";
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "90px";
   } else {
-    document.getElementById("subtitle").style.fontSize = "40pt";
-    document.getElementById("subtitle").style.transition = "2s";
+    document.getElementById("header").style.fontSize = "30px";
   }
 }
-window.addEventListener("scroll", scrollFunction)
 
-// document.querySelector("Guatemala").addEventListener("mouseenter", e =>
-// {  e.target.style.transform = "scale(1.1)";
-// });
-// document.querySelector("Guatemala").addEventListener("mouseleave", e =>
-// {  e.target.style.transform = "scale(1)";
-// });
+window.addEventListener("scroll", updateParallax)
