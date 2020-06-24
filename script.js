1ec92f785c745bd9d685d287b5bb255d1463fab2
gerardodelvalle <gerardo.delvalle@journalism.cuny.edu>
2020-06-24 15:18:08 -0400

    Changes to successfully start animating the page
    Changed a lot of javascript

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 const updateParallax = () => {
-  const scrollPosition = window.scrollY
-  const documentHeight = document.documentElement.scrollHeight
-  // - window.innerHeight
-  const pctScroll = scrollPosition/documentHeight;
-  const introtext = document.querySelector("h1")
-  const newDown = 1 * pctScroll * (document.documentElement.scrollHeight)
-  const newScale = 1 - ((pctScroll - 0.5) / 0.5)
-  if (pctScale <= 87){
-    newScale = 3;
-    marginTop = 10px;
-  }
-  const bg = document.querySelector("#opening")
-  bg.style.left = newDown + "px"
+  const scrollPosition = window.scrollY;
+  const documentHeight = document.documentElement.scrollHeight;
+  const pctScroll = scrollPosition / documentHeight;
 
-  const fg = document.querySelector("h1")
-  const newScale = 1 - ((pctScroll - 0.5) / 0.5)
-  let newScale
-  if (pctScale < 21){
-    newScale = 3;
+  const pctIntro = Math.min(pctScroll / 0.1, 1);
+  const pctSubIntro = Math.min(pctScroll / 0.05, 1);
+
+  const titleScale = 2 - pctIntro * 1;
+  const subTitleScale = 1 - pctSubIntro * 0.1;
+  const titleTop = 40 - pctIntro * 39;
+  if (scrollPosition < 784) {
+    document.querySelector(".header").style["background-color"] = "none";
   } else {
-    newScale = 1;
-    }
-  const newLeftFg = pctScroll * (document.documentElement.scrollWidth * 8)
-  fg.style.scale = newScale
+    document.querySelector(".header").style["background-color"] = "#0C1824";
 
-  console.log("updateParallax")
-  console.log("scrollPosition")
-  console.log("documentHeight")
-  console.log("pctScroll")
-}
+  };
 
-window.addEventListener("scroll", updateParallax)
+  document.querySelector(".header").style.transform = `scale(${titleScale})`;
+  document.querySelector(".header").style.top = `${titleTop}%`;
+  document.querySelector(".subtitle").style.transform = `scale(${subTitleScale})`;
+};
+
+ // + 818
+const movement02 = () => {
+  const scrollPosition02 = window.scrollY - 30;
+  const documentHeight02 = document.documentElement.scrollHeight;
+  const pctScroll02 = (scrollPosition02  / documentHeight02);
+
+  const pctIntro02 = Math.min(pctScroll02 / 0.1, 1);
+
+  const scaleTwo = pctIntro02 * 2;
+  const scaleTwoInvisible = 0;
+  // const twoTop =
+  if (scrollPosition02 < 782){
+  document.querySelector(".logline").style["opacity"] = 0;
+  document.querySelector(".logline").style.transform = `scale(${scaleTwo})`;
+}else   {
+  document.querySelector(".logline").style["opacity"] = 1;
+  document.querySelector(".logline").style.transform = `scale(${scaleTwo})`;
+  document.querySelector("#opening").style["opacity"] = 0;
+  // }
+  // ["scale"] = "0px";
+
+  console.log("pctIntro02", pctIntro02);
+  console.log("scrollPosition02", scrollPosition02);
+  console.log("documentHeight02", documentHeight02);
+
+};}
+
+
+window.addEventListener("scroll", updateParallax);
+window.addEventListener("scroll", movement02)
